60a8c02f5cbbeae22ee5169f7d8b9f38e756bc49
1402-NikhilSantosh <69449029+1402-NikhilSantosh@users.noreply.github.com>
2020-08-10 13:14:05 +0530

    file 2

---
+++ +ve nos in a list.py
@@ -0,0 +1,19 @@
+list1=[12,-7,5,64,-14]
+list2=[]
+i=0
+for i in list1:
+    if i>0:
+        
+        list2.append(i)
+print (list2)
+
+
+list3=[12,14,-95,3]
+list4=[]
+j=0
+for j in list3:
+    if j>0:
+        list4.append(j)
+print (list4)
+
+    
